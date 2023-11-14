389c6ca4b42c3c5585a2156ef241cc6054737719
Dersu <148701933+dersuprogg@users.noreply.github.com>
2023-11-14 12:19:08 +0500

    make function return value, instead of console logging

--- script.js
+++ script.js
@@ -10,22 +10,25 @@ function capitalize(str) {
   return str.slice(0, 1).toUpperCase() + str.slice(1,).toLowerCase();
 }
 
-const playerChoice = "paper";
-const computerChoice = getComputerChoice().toLowerCase();
+
 
 function playRound(playerSelection, computerSelection){
   if(playerSelection === "rock" && computerSelection === "paper"){
-    console.log("You Lose! Paper beats Rock");
+    return "You Lose! Paper beats Rock";
   }else if (playerSelection === "paper" && computerSelection === "scissors"){
-    console.log("You lose! Scissors beats Paper");
+    return "You lose! Scissors beats Paper";
   }else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("You lose! Rock beats Scissors");
+    return "You lose! Rock beats Scissors";
   }else if (playerSelection === computerSelection){
-    console.log(`It's a Tie, you botch chose ${capitalize(playerSelection)}`);
+    return `It's a Tie, you botch chose ${capitalize(playerSelection)}`;
   }else {
-    console.log(`You win! ${playerSelection} beats ${capitalize(computerSelection)}`);
+    return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
   }
 }
 
-playRound(playerChoice, computerChoice);
+
+const playerChoice = "paper";
+const computerChoice = getComputerChoice().toLowerCase();
+
+console.log(playRound(playerChoice, computerChoice));
 
