780497c5d06a31aaf03aca549dbca2a8b0dab393
Dersu <148701933+dersuprogg@users.noreply.github.com>
2023-11-15 13:34:26 +0500

    make a helper function to check a winner

--- script.js
+++ script.js
@@ -14,26 +14,40 @@ function capitalize(str) {
   return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-let playerScore = 0;
-let computerScore = 0;
+const player = {
+  name: "Player",
+  score: 0,
+};
+const computer = {
+  name: "Player",
+  score: 0,
+};
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "paper") {
-    computerScore++;
+    computer.score++;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerScore++;
+    computer.score++;
     return "You lose! Scissors beats Paper";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    computerScore++;
+    computer.score++;
     return "You lose! Rock beats Scissors";
   } else if (playerSelection === computerSelection) {
     return `It's a Tie, you botch chose ${capitalize(playerSelection)}`;
   } else {
-    playerScore++;
+    player.score++;
     return `You win! ${capitalize(playerSelection)} beats ${capitalize(
       computerSelection
     )}`;
+  }
+}
+
+function checkWinner(player1, player2) {
+  if (player1.score > player2.score) {
+    return player1.name;
+  } else {
+    return player2.name;
   }
 }
 
@@ -43,8 +57,9 @@ choicesBtn.forEach((btn) => {
     const computerChoice = getComputerChoice();
     const msg = playRound(playerChoice, computerChoice);
 
-    playerScoreSpan.textContent = playerScore;
-    computerScoreSpan.textContent = computerScore;
+    playerScoreSpan.textContent = player.score;
+    computerScoreSpan.textContent = computer.score;
+
     board.textContent = msg;
   });
 });
