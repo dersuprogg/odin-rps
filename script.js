44f407837b7106ff3c88901288f3d93362f14969
Dersu <148701933+dersuprogg@users.noreply.github.com>
2023-11-15 13:24:38 +0500

    make a basic ui

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choicesBtn = document.querySelectorAll(".btn");
+const playerScoreSpan = document.querySelector(".player-score");
+const computerScoreSpan = document.querySelector(".computer-score");
+const board = document.querySelector(".board");
 
-function getComputerChoice(){
-  let index = Math.floor((Math.random() * 3));
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  let index = Math.floor(Math.random() * 3);
   return choices[index];
 }
 
 function capitalize(str) {
-  return str.slice(0, 1).toUpperCase() + str.slice(1,).toLowerCase();
+  return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-function playRound(playerSelection, computerSelection){
-  if(playerSelection === "rock" && computerSelection === "paper"){
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
     return "You Lose! Paper beats Rock";
-  }else if (playerSelection === "paper" && computerSelection === "scissors"){
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
     return "You lose! Scissors beats Paper";
-  }else if (playerSelection === "scissors" && computerSelection === "rock") {
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
     return "You lose! Rock beats Scissors";
-  }else if (playerSelection === computerSelection){
+  } else if (playerSelection === computerSelection) {
     return `It's a Tie, you botch chose ${capitalize(playerSelection)}`;
-  }else {
-    return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+  } else {
+    playerScore++;
+    return `You win! ${capitalize(playerSelection)} beats ${capitalize(
+      computerSelection
+    )}`;
   }
 }
 
-const playerChoice = "paper";
+choicesBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const playerChoice = btn.innerText.toLowerCase();
+    const computerChoice = getComputerChoice();
+    const msg = playRound(playerChoice, computerChoice);
 
-function game(){
-  for(let i = 0; i <= 4; i++){
-    let computerChoice = getComputerChoice().toLowerCase();
-    let userChoice = prompt("Rock, Paper or Scissors? ").toLowerCase();
-    console.log(playRound(userChoice, computerChoice));
-  }
-}
-
-game();
+    playerScoreSpan.textContent = playerScore;
+    computerScoreSpan.textContent = computerScore;
+    board.textContent = msg;
+  });
+});
